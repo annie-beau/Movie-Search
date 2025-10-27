2eceabc61113deb0a87906fe18d1ff10c22131db
annie-beau <194310742+annie-beau@users.noreply.github.com>
2025-10-27 15:21:24 -0400

    Initial Commit

---
+++ Movie-Search/Movie_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_SearchApp.swift
+//  Movie-Search
+//
+//  Created by Annie Beaulieu on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
