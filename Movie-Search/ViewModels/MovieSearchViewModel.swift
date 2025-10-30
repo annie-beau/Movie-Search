379b5bae8dc4b71d8a240ff7b62744d4ba20ef7f
annie-beau <194310742+annie-beau@users.noreply.github.com>
2025-10-29 22:52:19 -0400

    adds pagination and data formatting

--- Movie-Search/ViewModels/MovieSearchViewModel.swift
+++ Movie-Search/ViewModels/MovieSearchViewModel.swift
@@ -9,19 +9,20 @@ import SwiftUI
 import Combine
 
 @Observable class MovieSearchViewModel {
-    // annie -- add an init?
     var movieList = [MovieModel]()
     var userInput: String = ""
+    var nextPage = 0
+    var currentPage = 0
+    var totalPages = 0
     
-    // annie - add pagination
-    func getMovies() async {
+    func getMovies(page: Int) async {
         let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as! String
         var components = URLComponents(string: "https://api.themoviedb.org/3/search/movie")!
         components.queryItems = [
             URLQueryItem(name: "api_key", value: apiKey),
             URLQueryItem(name: "language", value: "en-US"),
-            URLQueryItem(name: "query", value: userInput), // annie - can't hard code this
-            URLQueryItem(name: "page", value: "1"), // annie - can't hard code this
+            URLQueryItem(name: "query", value: userInput),
+            URLQueryItem(name: "page", value: String(page)),
             URLQueryItem(name: "include_adult", value: "false")
         ]
         guard let url = components.url else {
@@ -30,21 +31,53 @@ import Combine
         }
         
         do {
-            // get data
             let (data, response) = try await URLSession.shared.data(from: url)
-            
-            // decode json to model format
             if let results = try? JSONDecoder().decode(MovieListModel.self, from: data) {
-                movieList = results.results
+                currentPage = results.page
+                if currentPage == 1 {
+                    movieList = results.results
+                    totalPages = results.totalPages
+                } else {
+                    movieList.append(contentsOf: results.results)
+                }
             }
+            nextPage = currentPage + 1
         } catch {
             print("Fetch error: ", error)
         }
     }
     
+    func loadNextPage() async {
+        guard nextPage < totalPages else {
+            return
+        }
+        await getMovies(page: nextPage)
+    }
+        
+    
     func createImageUrl(path: String?) -> URL? {
         guard let path = path else { return nil }
         let baseURL = "https://image.tmdb.org/t/p/original/"
         return URL(string: baseURL + path)
+    }
+    
+    func formatDate(dateString: String, outputFormat: String) -> String {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-mm-dd"
+        guard let date = inputFormatter.date(from: dateString) else {
+               return "Unknown"
+           }
+        
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = outputFormat
+        return outputFormatter.string(from: date)
+    }
+    
+    func formatRating(rating: Float) -> String {
+        rating == 0 ? "Unknown" : String(format: "%.1f", rating)
+    }
+    
+    func getRatingScale(rating: Float) -> Float {
+        return rating/10
     }
 }
--- Movie-Search/Views/MovieSearchView.swift
+++ Movie-Search/Views/MovieSearchView.swift
@@ -11,21 +11,68 @@ struct MovieSearchView: View {
     @State private var viewModel = MovieSearchViewModel()
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Movie Search")
-                .font(.title)
-                .padding()
-            TextField("Search", text: $viewModel.userInput)
-                .onSubmit {
-                    Task {
-                        await viewModel.getMovies()
-                    }
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Divider()
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundStyle(Color.gray)
+                        .padding()
+                    TextField("Search", text: $viewModel.userInput)
+                        .onChange(of: viewModel.userInput) {
+                            Task {
+                                await viewModel.getMovies(page: 1)
+                            }
+                        }
+                    Button {
+                        viewModel.userInput = ""
+                        viewModel.movieList = []
+                        
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundStyle(Color.gray)
+                    }.padding()
                 }
-            List { // annie -- to switch to a ScrollView?
-                ForEach(viewModel.movieList, id: \.id) { movie in
-                    MovieListView(imageUrl: viewModel.createImageUrl(path: movie.posterPath), title: movie.title, releaseDate: movie.releaseDate)
+                .frame(height: 25)
+                .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                .background(Color(red: 0.9, green: 0.9, blue: 0.9).clipShape(RoundedRectangle(cornerRadius:10)))
+                .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                
+                Divider()
+                if viewModel.movieList == [] {
+                    VStack {
+                        Spacer()
+                        Text("No results")
+                        Spacer()
+                    }.frame(maxWidth: .infinity)
+                } else {
+                    List {
+                        ForEach(viewModel.movieList, id: \.id) { movie in
+                            ZStack(alignment: .leading) {
+                                    MovieListItemView(imageUrl: viewModel.createImageUrl(path: movie.posterPath), title: movie.title, releaseDate: viewModel.formatDate(dateString: movie.releaseDate, outputFormat: "yyyy"))
+                                    .onAppear {
+                                        if movie == viewModel.movieList.last {
+                                            Task {
+                                                await viewModel.loadNextPage()
+                                            }
+                                        }
+                                    }
+                                NavigationLink(value: movie) {
+                                    EmptyView()
+                                }.opacity(0)
+                            }
+                        }
+                    }.listStyle(PlainListStyle())
+                        .navigationDestination(for: MovieModel.self) { movie in
+                            MovieDetailsView(title: movie.title,
+                                             date: viewModel.formatDate(dateString: movie.releaseDate, outputFormat: "MMMM d, yyyy"),
+                                             imageUrl: viewModel.createImageUrl(path: movie.posterPath),
+                                             rating: viewModel.formatRating(rating: movie.rating),
+                                             overview: movie.overview,
+                                             progress: viewModel.getRatingScale(rating: movie.rating))
+                        }
                 }
-            }
+            }.navigationTitle("Movie Search")
         }
     }
 }
